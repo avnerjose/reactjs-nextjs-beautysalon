972aa23cea45ee6b5422783dc57074981289f150
avnerjose <avnerj.g.r@gmail.com>
2022-04-22 00:27:48 -0300

    First commit

---
+++ src/themes/theme.ts
@@ -0,0 +1,19 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    primary: "#69B99D",
+    secondary: "#CCF4E6",
+    dark: "#0F241D",
+    gray: {
+      dark: "#767676",
+      light: "#E4E4E4",
+    },
+    background: {
+      1: "#F9F9F9",
+      2: "#FFFFFF",
+    },
+  },
+});
+
+export { theme };
