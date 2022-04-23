875180e65ef26d1d095a79ea84323d9c56ff6aff
avnerjose <avnerj.g.r@gmail.com>
2022-04-23 15:40:02 -0300

    Creating home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+
+import { Footer, Navbar } from "../components";
+import {
+  AboutUsSection,
+  ContactSection,
+  HeroSection,
+  ServicesSection,
+  TestimonialsSection,
+} from "../components/sections";
 
 const Home: NextPage = () => {
-  return <h1>Teste</h1>;
+  return (
+    <>
+      <Head>
+        <title>Beauty Salon | Home</title>
+      </Head>
+      <Navbar />
+      <HeroSection />
+      <AboutUsSection />
+      <ServicesSection />
+      <TestimonialsSection />
+      <ContactSection />
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
